949b270020cb55c19f8680bf04644e463387a8c5
Ansh27122006 <kauransh2006@gmail.com>
2026-05-12 10:58:58 +0530

    Updated cors origins to allow frontend

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,8 @@ const io = new Server(server, {
   cors: {
     origin:
       process.env.FRONTEND_URL ||
-      "http://localhost:5173" ||
-      "https://ondemand.vercel.app",
+      "https://ondemand.vercel.app" ||
+      "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
